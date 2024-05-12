99424312507c868407e27fb5029450d68badb3cf
lucifer-14 <killerlucifer14@gmail.com>
2024-05-12 20:32:01 +0700

    Added control buttons and a sub container for login feedback

--- forms/auth_forms/login_form.py
+++ forms/auth_forms/login_form.py
@@ -1,11 +1,15 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
+import forms.main_menu_form as FMMF
+import socket_handle.auth_handle.login as SAL
+
 
 class Login_Form(ttk.Frame):
     def __init__(self, master) -> None:
         # super.__init__(master)
         master.pack(fill=BOTH, expand=YES)      # master = main_container
+        self.master = master
 
         w_width = master.winfo_screenwidth()
         w_height = master.winfo_screenheight()
@@ -17,51 +21,88 @@ class Login_Form(ttk.Frame):
         self.login_container.pack(fill=BOTH, expand=YES)
         
         title = ttk.Label(self.login_container, text="Login Form", font=("Garamond", 24), anchor=CENTER)
-        title.pack(fill=X, pady=(10,50), expand=YES, anchor=CENTER)
+        title.pack(fill=X, pady=(10,40), expand=YES, anchor=CENTER)
+
+        subcontainer = ttk.Frame(self.login_container)
+        subcontainer.pack(fill=X, expand=YES, pady=2)
+
+        hidden_lbl = ttk.Label(subcontainer, width=18)
+        hidden_lbl.pack(side=LEFT, padx=5)
+        self.hidden_comment = ttk.Label(subcontainer, font=("Garamond", 12))
+        self.hidden_comment.pack(fill=X, padx=5, expand=YES)
 
         self.create_form_entry("Username or Email:", self.username)
         self.create_form_entry("Password:", self.password, is_password=True)
+        self.create_control_buttons()
 
 
     def create_form_entry(self, label: str, variable: ttk.StringVar, is_password: bool = False) -> None:
         container = ttk.Frame(self.login_container)
-        container.pack(fill=X, expand=YES, pady=5)
+        container.pack(fill=X, expand=YES, pady=10)
         
-        lbl = ttk.Label(master=container, text=label.title(), width=20)
+        lbl = ttk.Label(master=container, text=label.title(), width=18)
         lbl.pack(side=LEFT, padx=5)
         # lbl.grid(row=0, column=0, padx=(5, 0), sticky=E)
 
         if is_password:
-            ent = ttk.Entry(master=container, textvariable=variable, show="*")
+            ent = ttk.Entry(master=container, textvariable=variable, show="*", width=30)
         else:
-            ent = ttk.Entry(master=container, textvariable=variable)
+            ent = ttk.Entry(master=container, textvariable=variable, width=30)
         
         ent.pack(side=LEFT, padx=5, fill=X, expand=YES)
         # ent.grid(row=0, column=1, padx=(0, 5), sticky=W+E)
 
 
     def create_control_buttons(self):
-        container = ttk.Frame(self)
-        container.pack(fill=X, expand=YES, pady=(15, 10))
+        container = ttk.Frame(self.login_container)
+        container.pack(fill=X, expand=YES, pady=(25, 10))
 
         sub_btn = ttk.Button(
             master=container,
             text="Login",
-            command=self.on_submit,
+            command=self.on_login,
             bootstyle=SUCCESS,
-            width=6,
+            width=8,
         )
-        sub_btn.pack(side=RIGHT, padx=5)
+        sub_btn.pack(side=RIGHT, padx=10)
         sub_btn.focus_set()
 
         cnl_btn = ttk.Button(
             master=container,
             text="Go Back",
-            command=self.on_cancel,
+            command=self.on_back,
             bootstyle=DANGER,
-            width=6,
+            width=8,
         )
-        cnl_btn.pack(side=RIGHT, padx=5)
+        cnl_btn.pack(side=RIGHT, padx=10)
+
+
+    def clear_content(self) -> None:
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+
+
+    def on_login(self) -> None:
+        login_handle = SAL.Login(self.username.get(), self.password.get())
+        login_res = login_handle.login()
+        
+        self.hidden_comment.config(text=login_res['comment'])
+
+        if (login_res['is_login_success']):
+            self.hidden_comment.config(bootstyle=SUCCESS)
+            # continue after login page
+            pass
+        else:
+            self.hidden_comment.config(bootstyle=DANGER)
+
+        print(login_res['comment'])
+        
+
+    def on_back(self):
+        self.clear_content()
+        FMMF.Main_Menu_Form(self.master)
+        pass
+
 
 if __name__ == "__main__":
 
