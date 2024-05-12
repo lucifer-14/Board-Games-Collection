61db8f7e80bf282b46df39de22b6c650859f1eae
lucifer-14 <killerlucifer14@gmail.com>
2024-05-12 20:35:08 +0700

    Added client side request sender

---
+++ socket_handle/send_request.py
@@ -0,0 +1,39 @@
+""" 
+This is client-side
+"""
+
+import socket
+import json
+import socket_handle.establish_connection as ESTA_CONN
+
+class Send_Request:
+    def __init__(self):
+        
+        esta = ESTA_CONN.Establish_Connection()
+        self.conn = esta.esta_conn()
+
+    def send_req(self, req_type: str, req_uri: str, data: dict) -> dict:
+        full_data = {"request-type": req_type,
+                     "request-URI": req_uri,
+                     "data": data}
+        full_data = json.dumps(full_data)
+        # print(self.conn)
+        # print(type(self.conn))
+        self.conn.sendall(full_data.encode('utf-8'))
+        print("data sent from client")
+        
+        # return_data = b""
+        # while True:
+            # print("reconstructing....")
+            # data = self.conn.recv(1)
+            # if not data:
+                # break
+            # return_data += data
+        print("reconstructing....")
+        return_data = self.conn.recv(4096)
+
+        print(return_data.decode('utf-8'))
+
+        return json.loads(return_data.decode('utf-8'))
+        # while True:
+        #     print("hi")
