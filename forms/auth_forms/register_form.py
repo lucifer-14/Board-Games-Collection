6bbc8926ab100c2c36c825616c5a8d9b90490ddf
lucifer-14 <killerlucifer14@gmail.com>
2024-05-13 11:10:38 +0700

    Added register form

---
+++ forms/auth_forms/register_form.py
@@ -0,0 +1,136 @@
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+import forms.main_menu_form as FMMF
+import socket_handle.auth_handle.register as SAR
+import utils.config_handler as CONF_H
+
+
+class Register_Form(ttk.Frame):
+    def __init__(self, master) -> None:
+        # super.__init__(master)
+        master.pack(fill=BOTH, expand=YES)      # master = main_container
+        self.master = master
+
+        self.conf_h = CONF_H.Config_Handler()
+        # REMEMBER_ME = False if self.conf_h.get_config('REMEMBER_ME').lower() == "false" else True
+
+        w_width = master.winfo_screenwidth()
+        w_height = master.winfo_screenheight()
+
+        self.username = ttk.StringVar(value="")
+        self.email = ttk.StringVar(value="")
+        self.password = ttk.StringVar(value="")
+        self.re_password = ttk.StringVar(value="")
+        # self.remember_me_bool = ttk.BooleanVar(value=REMEMBER_ME)
+
+        self.register_container = ttk.Frame(master, padding=(w_width / 8, w_height / 10))
+        self.register_container.pack(fill=BOTH, expand=YES)
+        
+        title = ttk.Label(self.register_container, text="Register Form", font=("Garamond", 24), anchor=CENTER)
+        title.pack(fill=X, pady=(10,40), expand=YES, anchor=CENTER)
+
+        subcontainer = ttk.Frame(self.register_container)
+        subcontainer.pack(fill=X, expand=YES, pady=2)
+
+        hidden_lbl = ttk.Label(subcontainer, width=18)
+        hidden_lbl.pack(side=LEFT, padx=5)
+        self.hidden_comment = ttk.Label(subcontainer, font=("Garamond", 12))
+        self.hidden_comment.pack(fill=X, padx=5, expand=YES)
+
+        self.create_form_entry("Username:", self.username)
+        self.create_form_entry("Email:", self.email)
+        self.create_form_entry("Password:", self.password, is_password=True)
+        self.create_form_entry("Re-Type Password:", self.re_password, is_password=True)
+
+        # subcontainer1 = ttk.Frame(self.login_container)
+        # subcontainer1.pack(fill=X, expand=YES, pady=2)
+
+        # hidden_lbl1 = ttk.Label(subcontainer1, width=18)
+        # hidden_lbl1.pack(side=LEFT, padx=5)
+
+        # checkbox = ttk.Checkbutton(subcontainer1, text="Remember Me", variable=self.remember_me_bool, command=self.on_remember_me_click)
+        # checkbox.pack(fill=X, padx=5,expand=YES)
+        self.create_control_buttons()
+
+
+    def create_form_entry(self, label: str, variable: ttk.StringVar, is_password: bool = False) -> None:
+        container = ttk.Frame(self.register_container)
+        container.pack(fill=X, expand=YES, pady=10)
+        
+        lbl = ttk.Label(master=container, text=label.title(), width=18)
+        lbl.pack(side=LEFT, padx=5)
+        # lbl.grid(row=0, column=0, padx=(5, 0), sticky=E)
+
+        if is_password:
+            ent = ttk.Entry(master=container, textvariable=variable, show="*", width=30)
+        else:
+            ent = ttk.Entry(master=container, textvariable=variable, width=30)
+        
+        ent.pack(side=LEFT, padx=5, fill=X, expand=YES)
+        # ent.grid(row=0, column=1, padx=(0, 5), sticky=W+E)
+
+
+    def create_control_buttons(self):
+        container = ttk.Frame(self.register_container)
+        container.pack(fill=X, expand=YES, pady=(25, 10))
+
+        sub_btn = ttk.Button(
+            master=container,
+            text="Register",
+            command=self.on_register,
+            bootstyle=SUCCESS,
+            width=8,
+        )
+        sub_btn.pack(side=RIGHT, padx=10)
+        sub_btn.focus_set()
+
+        cnl_btn = ttk.Button(
+            master=container,
+            text="Go Back",
+            command=self.on_back,
+            bootstyle=DANGER,
+            width=8,
+        )
+        cnl_btn.pack(side=RIGHT, padx=10)
+
+
+    def clear_content(self) -> None:
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+
+
+    def on_register(self) -> None:
+        
+        login_handle = SAL.Login(self.username.get(), self.password.get())
+        login_res = login_handle.login_request()
+        
+        self.hidden_comment.config(text=login_res['comment'])
+
+        if (login_res['is_login_success']):
+            self.hidden_comment.config(bootstyle=SUCCESS)
+            # continue after login page
+            pass
+        else:
+            self.hidden_comment.config(bootstyle=DANGER)
+    
+    # def on_remember_me_click(self):
+        
+    #     self.conf_h.set_config("REMEMBER_ME", self.remember_me_bool.get())
+
+
+
+    def on_back(self):
+        self.clear_content()
+        FMMF.Main_Menu_Form(self.master)
+        
+
+
+if __name__ == "__main__":
+
+    app = ttk.Window(title="Board Games Collection", themename="superhero", resizable=(False, False))
+    main_container = ttk.Frame(app)
+    main_container.pack(fill=BOTH, expand=YES)
+    Login_Form(main_container)
+
+    app.mainloop()
