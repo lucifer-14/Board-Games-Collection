2f721dece843cf7d9631980eb42247690f2d9e71
lucifer-14 <killerlucifer14@gmail.com>
2024-05-14 21:44:29 +0700

    Added after_login menu

--- board_game_collection.py
+++ board_game_collection.py
@@ -3,20 +3,28 @@ from ttkbootstrap.constants import *
 
 import socket_handle
 import sql_handle
-import forms.main_menu_form as FMMF
+import forms.main_menu_form as FMF
+import forms.menu_forms.menu_form as FMMF
 import utils.config_handler as CONF_H
 
 class Main_Form(ttk.Frame):
     def __init__(self, master) -> None:
+        super().__init__(master)
+
+        conf_h = CONF_H.Config_Handler()
+
         w_width = master.winfo_screenwidth()
         w_height = master.winfo_screenheight()
-        super().__init__(master)
+
         self.pack(fill=BOTH, expand=YES)
 
         self.main_container = ttk.Frame(self)
         self.main_container.pack(fill=BOTH, expand=YES)
 
-        FMMF.Main_Menu_Form(self.main_container)
+        if conf_h.get_config('CURRENT_SESSION'):
+            FMMF.Menu_Form(self.main_container)
+        else:
+            FMF.Main_Menu_Form(self.main_container)
 
 
 
